df58c3cffd58ec441377cc8a32ee10bd5221f247
Dillon Eng <dillon@MacBook-Pro.local>
2016-07-16 13:26:20 -0700

    add printing binary

---
+++ sort/printbin.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int num;
+    sscanf(argv[1], "%i", &num);
+
+    int i = 31;
+    while (i >= 0)
+    {
+        printf("%d", (num >> i) & 1);
+        i--;
+    }
+    printf("\n");
+}
